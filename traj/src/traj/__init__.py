4f40b49beedbea517f29f516e4eebfae1dbfec31
mahmoud-a-ali <mahmud.a.aly@gmail.com>
2020-03-05 11:07:45 +0100

    add new modules

--- traj/src/traj/__init__.py
+++ traj/src/traj/__init__.py
@@ -6,6 +6,8 @@ import plot
 from trajectory import trajectory_for_path
 
 from traj_segment import fit_traj_segment
+from traj_segment import calculate_jerk_sign_and_duration
+
 from segment_planning import traj_segment_planning
 from segment_planning import calculate_minPos_reachAcc_maxJrkTime_maxAccTime_to_final_vel
 
@@ -14,3 +16,13 @@ from cubic_eq_roots import real_roots_cubic_eq
 from cubic_eq_roots import quad_eq_real_root
 from cubic_eq_roots import min_positive_root2
 from cubic_eq_roots import min_positive_root3
+
+
+
+from max_reachable_vel import max_reachable_vel
+from param_max_reachable_vel import set_stp_pts_to_zero
+from param_max_reachable_vel import find_max_estimated_vel_per_path
+from param_max_reachable_vel import find_max_estimated_vel_per_ndof_path
+
+
+from sample_segment import sample_segment
