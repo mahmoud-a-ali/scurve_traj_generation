22f52caf32192ab9fce546d21a23b6aa6f3fbf2a
mahmoud-a-ali <mahmud.a.aly@gmail.com>
2020-03-25 14:35:28 +0100

    change funcs names

--- traj/src/traj/__init__.py
+++ traj/src/traj/__init__.py
@@ -11,9 +11,11 @@ from traj_segment import fit_traj_segment
 from traj_segment import calculate_jerk_sign_and_duration
 
 from segment_planning import traj_segment_planning
-from segment_planning import calculate_minPos_reachAcc_maxJrkTime_maxAccTime_to_final_vel
+from segment_planning import calculate_min_pos_reached_acc_jrk_time_acc_time_to_reach_final_vel
 
 from plot_traj_segment import plot_traj_segment
+
+
 from cubic_eq_roots import real_roots_cubic_eq
 from cubic_eq_roots import quad_eq_real_root
 from cubic_eq_roots import min_positive_root2
@@ -21,10 +23,12 @@ from cubic_eq_roots import min_positive_root3
 
 
 
-from max_reachable_vel import max_reachable_vel
-from param_max_reachable_vel import set_stp_pts_to_zero
-from param_max_reachable_vel import find_max_estimated_vel_per_path
-from param_max_reachable_vel import find_max_estimated_vel_per_ndof_path
+from max_reachable_vel import max_reachable_vel_per_segment
+
+
+from param_max_reachable_vel import set_velocities_at_stop_points_to_zero
+from param_max_reachable_vel import reachable_vel_at_each_waypoint_one_dof_path_case
+from param_max_reachable_vel import reachable_vel_at_each_waypoint_multi_dof_path_case
 
 
 from sample_segment import sample_segment
