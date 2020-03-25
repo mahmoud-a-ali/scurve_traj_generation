96bceb59b2fb0727b9e875b3fe909371d187499e
mahmoud-a-ali <mahmud.a.aly@gmail.com>
2020-03-25 14:33:29 +0100

    func/var naming convention && docstrings

--- traj/src/traj/segment_planning.py
+++ traj/src/traj/segment_planning.py
@@ -6,8 +6,13 @@ import rospy
 import math
 import cubic_eq_roots as rt
 
-### calculate min_position and localMaxAcc to reach absMaxVel "v_max"
-def calculate_minPos_localMaxAcc_to_absMaxVel(v, vm, am, jm):
+
+def calculate_min_pos_reached_acc_to_reach_max_vel(v, vm, am, jm):
+    '''
+    This function calculates the following variables to reach the absolute Maximum Velocity "vm"  starting with initial_velocity "v":
+    1. the minimum position required to reach the maximum absolute velocity "vm"
+    2. the acceleration "acc" that has been reached to reach the maximum absolute velocity starting with "v"
+    '''
     p0=0.0
     a0=0.0
     v0=v
@@ -38,14 +43,17 @@ def calculate_minPos_localMaxAcc_to_absMaxVel(v, vm, am, jm):
     p6 =                  a5*ta*ta/2.0  + v5*ta + p5;
     p7 =  jm*t*t*t/6.0 +  a6*t*t/2.0    + v6*t  + p6;
     
-    minPos_to_absMaxVel = p7
-    localMaxAcc_to_absMaxVel = ar
-    return minPos_to_absMaxVel, localMaxAcc_to_absMaxVel
+    min_pos_to_max_vel = p7
+    reached_acc_to_max_vel = ar
+    return min_pos_to_max_vel, reached_acc_to_max_vel
     
 
 
-### calculate required acc to achieve a given position difference
-def calculate_Acc_No_constAccPhase(Dp, v, vm, am ,jm):
+def calculate_reached_acc_in_case_no_const_acc_phase(Dp, v, vm, am ,jm):
+    '''
+    This function calculates the acceleration that has been reached to move the joint a position equals to "DP" starting with velocity "v"
+    this considers the case where there is no a constant acceleration phase.
+    '''
     p0=0.0
     v0=v
      
@@ -66,8 +74,14 @@ def calculate_Acc_No_constAccPhase(Dp, v, vm, am ,jm):
     return ar
  
 
-### calculate minPos and constAccTime to reach both the absMaxAcc "a_max" and the absMaxVel "v_max"
-def calculate_minPos_constAccTime_to_absMaxAcc_absMaxVel(v, vm, am, jm):
+
+def calculate_min_pos_const_acc_time_to_reach_max_acc_and_max_vel(v, vm, am, jm):
+    '''
+    This function calculates the following variables to reach the absolute Maximum Velocity "vm"  
+    starting with initial_velocity "v" considering that the absolute maximum acceleration "am" will be reached:
+       1. the minimum position required to reach the maximum absolute velocity "vm"
+       2. the acceleration_phase time "ta" to reach the maximum absolute velocity "vm"
+    '''
     p0=0.0
     a0=0.0
     v0=v
@@ -97,13 +111,16 @@ def calculate_minPos_constAccTime_to_absMaxAcc_absMaxVel(v, vm, am, jm):
     p6 =                  a5*ta*ta/2.0  + v5*ta + p5;
     p7 =  jm*t*t*t/6.0 +  a6*t*t/2.0    + v6*t  + p6;
     
-    minPos_to_absMaxVel= p7-p0
+    min_pos_to_max_vel= p7-p0
     t_max_acc = ta
-    return minPos_to_absMaxVel, t_max_acc
+    return min_pos_to_max_vel, t_max_acc
 
 
-## calculate minPos to reach the absMaxAcc (a_max)
-def calculate_minPos_to_absMaxAcc(v, vm, am, jm):
+
+def calculate_min_pos_to_reach_max_acc(v, vm, am, jm):
+    '''
+    This function calculates the minimum position required to reach the absolute maximum acceleration "am", starting with initial velocity "v"
+    '''
     p0=0.0
     a0=0.0
     v0=v
@@ -133,12 +150,17 @@ def calculate_minPos_to_absMaxAcc(v, vm, am, jm):
     p6 =                  a5*ta*ta/2.0  + v5*ta + p5;
     p7 =  jm*t*t*t/6.0 +  a6*t*t/2.0    + v6*t  + p6;
     
-    minPos_absMaxAcc= p7-p0
-    return minPos_absMaxAcc
+    min_pos_to_max_acc= p7-p0
+    return min_pos_to_max_acc
     
 
-## calculate the time of the const_acc_phase to reach required pos_diff, in case that the absMaxVel won't be reached
-def calculate_absMaxAccTime(Dp, v, vm, am, jm):
+
+def calculate_const_acc_time(Dp, v, vm, am, jm):
+    '''
+    This function calculates the acceleration_phase time "ta" which is required to move the joint 
+    to new position equal to the current position plus a position difference "DP" starting with initial velocity "v"
+    '''
+
     # here we wont reach max vel but still we reach max acc     
     p0=0.0
     v0=v
@@ -155,14 +177,19 @@ def calculate_absMaxAccTime(Dp, v, vm, am, jm):
         ta = rt.min_positive_root2(rt1, rt2)
     elif n_rts ==3:
         ta = rt.min_positive_root3(rt1, rt2, rt3)    
-    absMaxAccTime = ta
-    return absMaxAccTime
+    const_acc_time = ta
+    return const_acc_time
     
     
 
 
-### calculate minPos to reach final_vel starting with initial_vel
-def calculate_minPos_reachAcc_maxJrkTime_maxAccTime_to_final_vel(v0, vf, vm, am, jm):
+def calculate_min_pos_reached_acc_jrk_time_acc_time_to_reach_final_vel(v0, vf, vm, am, jm):
+    '''
+    This function calculates the following variables to reach the final Velocity "vf"  starting with initial_vel "v0":
+    1. the minimum position required to reach the final Velocity "vf"
+    2. the acceleration "acc" that has been reached to reach the final Velocity "vf" starting with "v0"
+    3. phases times to reach the final Velocity "vf": jerk_phase time "tj" and acceleration_phase time "ta" 
+    '''
     p0=0.0
     a0=0.0
 
@@ -189,7 +216,7 @@ def calculate_minPos_reachAcc_maxJrkTime_maxAccTime_to_final_vel(v0, vf, vm, am,
     
     if abs(a2)<=abs(am):
         acc_to_vf = a2 #math.copysign(a2,  (vf-v0) )
-        minPos_to_vf = p3 #math.copysign(p3,  (vf-v0) )
+        min_pos_to_vf = p3 #math.copysign(p3,  (vf-v0) )
     else:
         tj =abs(am/jm) 
         ta= ( abs(vf-v0) - abs((am**2/jm)) )/am        
@@ -205,65 +232,68 @@ def calculate_minPos_reachAcc_maxJrkTime_maxAccTime_to_final_vel(v0, vf, vm, am,
         p2 =                  a1*ta*ta/2.0  + v1*ta + p1;
         p3 = -jm*t*t*t/6.0 +  a2*t*t/2.0    + v2*t  + p2;  
         acc_to_vf = a2 #math.copysign(a2,  (vf-v0) )
-        minPos_to_vf = p3 #math.copysign(p3,  (vf-v0) )
-    return minPos_to_vf, acc_to_vf, tj, ta
+        min_pos_to_vf = p3 #math.copysign(p3,  (vf-v0) )
+    return min_pos_to_vf, acc_to_vf, tj, ta
          
          
          
 
     
 
-#### planing motion profile in case of "vf=v0", it returns times for each phase: max_jrk, max_acc, max_vel     
 def equal_vel_case_planning (pos_diff, v, abs_max_vel, abs_max_acc, abs_max_jrk):
-    # check if a const_acc phase is required or not by calcuating the localMaxAcc_to_absMaxVel to reach vf
-    minPos_to_absMaxVel, localMaxAcc_to_absMaxVel = calculate_minPos_localMaxAcc_to_absMaxVel(v, abs_max_vel, abs_max_acc, abs_max_jrk)
+    '''
+    this function selects a motion profile for a trajectory segment in case of the final veoclity equals the starting velocity, both equal "v" 
+    '''
 
-    # check if localMaxAcc_to_absMaxVel < abs_max_acc: 
+    # check if a const_acc phase is required or not by calcuating the reached_acc_to_max_vel to reach vf
+    min_pos_to_max_vel, reached_acc_to_max_vel = calculate_min_pos_reached_acc_to_reach_max_vel(v, abs_max_vel, abs_max_acc, abs_max_jrk)
+
+    # check if reached_acc_to_max_vel < abs_max_acc: 
     #if yes: then no const_acc phase, check if a const_vel phase is required or not (to satisfy pos_diff) 
-    if(localMaxAcc_to_absMaxVel<= abs_max_acc):
+    if(reached_acc_to_max_vel<= abs_max_acc):
         rospy.logdebug("case a: maxAcc won't be reached !  /\\/ ")
-        localMaxVel = abs_max_vel
-        localMaxAcc = localMaxAcc_to_absMaxVel      
-        t_max_jrk = localMaxAcc_to_absMaxVel/abs_max_jrk
+        reached_vel = abs_max_vel
+        reached_acc = reached_acc_to_max_vel      
+        t_max_jrk = reached_acc_to_max_vel/abs_max_jrk
         t_max_acc = 0.0
         t_max_vel = 0.0
         
-        if(pos_diff > minPos_to_absMaxVel):
+        if(pos_diff > min_pos_to_max_vel):
             rospy.logdebug("\n >>> case a1: require const_vel_phase=zero_acc_phase [ /\-----\/ ]" )
-            t_max_vel= (pos_diff - minPos_to_absMaxVel )/ abs_max_vel
+            t_max_vel= (pos_diff - min_pos_to_max_vel )/ abs_max_vel
         else:
             rospy.logdebug("\n >>> case a2: calculate Acc corresponds to pos_diff [ /\\/ ]")
-            acc = calculate_Acc_No_constAccPhase(pos_diff, v, abs_max_vel, abs_max_acc, abs_max_jrk)
+            acc = calculate_reached_acc_in_case_no_const_acc_phase(pos_diff, v, abs_max_vel, abs_max_acc, abs_max_jrk)
             t_max_jrk = acc/abs_max_jrk
          
         
     #if  no: check if a const_acc phase is required or not (to satisfy pos_diff) 
-    elif(localMaxAcc_to_absMaxVel > abs_max_acc):
+    elif(reached_acc_to_max_vel > abs_max_acc):
         rospy.logdebug("case b: maxAcc will be reached !  /'''\\.../")
-        minPos_to_absMaxVel, t_max_acc = calculate_minPos_constAccTime_to_absMaxAcc_absMaxVel(v, abs_max_vel, abs_max_acc, abs_max_jrk)
-        localMaxVel = abs_max_vel
-        localMaxAcc = abs_max_acc    
+        min_pos_to_max_vel, t_max_acc = calculate_min_pos_const_acc_time_to_reach_max_acc_and_max_vel(v, abs_max_vel, abs_max_acc, abs_max_jrk)
+        reached_vel = abs_max_vel
+        reached_acc = abs_max_acc    
         t_max_jrk = abs_max_acc/abs_max_jrk
         t_max_vel = 0.0
         
-        if(pos_diff >= minPos_to_absMaxVel):
+        if(pos_diff >= min_pos_to_max_vel):
             rospy.logdebug("\n >>> case b1: require const_vel_phase=zero_acc_phase [ /```\------\.../ ]" )
-            t_max_vel= (pos_diff - minPos_to_absMaxVel )/ abs_max_vel
+            t_max_vel= (pos_diff - min_pos_to_max_vel )/ abs_max_vel
         else:
-            minPos_absMaxAcc = calculate_minPos_to_absMaxAcc(v, abs_max_vel, abs_max_acc, abs_max_jrk)
-            rospy.logdebug("case b2: minPos_absMaxAcc= {}, Dp= {} ".format(minPos_absMaxAcc, pos_diff) )
-            if(pos_diff >= minPos_absMaxAcc):
-                rospy.logdebug( "\n >>> case b2a: calculate absMaxAccTime-localMaxVel corresponds to pos_diff [ /````\\..../ ]" )
-                absMaxAccTime = calculate_absMaxAccTime(pos_diff, v, abs_max_vel, abs_max_acc, abs_max_jrk)
-                t_max_acc = absMaxAccTime
+            min_pos_to_max_acc = calculate_min_pos_to_reach_max_acc(v, abs_max_vel, abs_max_acc, abs_max_jrk)
+            rospy.logdebug("case b2: min_pos_to_max_acc= {}, Dp= {} ".format(min_pos_to_max_acc, pos_diff) )
+            if(pos_diff >= min_pos_to_max_acc):
+                rospy.logdebug( "\n >>> case b2a: calculate acc_time-reached_vel corresponds to pos_diff [ /````\\..../ ]" )
+                acc_time = calculate_const_acc_time(pos_diff, v, abs_max_vel, abs_max_acc, abs_max_jrk)
+                t_max_acc = acc_time
             else:
                 rospy.logdebug( "\n >>> case b2b: calculate acc corresponds to pos_diff [ /\\/ ]")
-                acc = calculate_Acc_No_constAccPhase(pos_diff, v, abs_max_vel, abs_max_acc, abs_max_jrk)
+                acc = calculate_reached_acc_in_case_no_const_acc_phase(pos_diff, v, abs_max_vel, abs_max_acc, abs_max_jrk)
                 t_max_jrk = acc/abs_max_jrk
                 t_max_acc = 0.0
                 t_max_vel = 0.0
            
-    return t_max_jrk, t_max_acc, t_max_vel, localMaxVel, localMaxAcc
+    return t_max_jrk, t_max_acc, t_max_vel, reached_vel, reached_acc
 
 
 
@@ -271,13 +301,15 @@ def equal_vel_case_planning (pos_diff, v, abs_max_vel, abs_max_acc, abs_max_jrk)
 
 ### the main function to plan motion profile for a general_velocity-to-general_velocity segment
 def traj_segment_planning(p_start, p_end, abs_v_start, abs_v_end, abs_max_vel, abs_max_acc, abs_max_jrk):
-    """
-    plan motion profile for a traj segment with given start and end velocities/positions, considering the start and end accelerations/jerks are zeros!        
-    """    
+    '''
+    this function selects a motion profile for a trajectory segment with a given start and end velocities/positions, 
+    considering the start and end accelerations/jerks are zeros!        
+    '''
+
     #calculate min_pos required to reach vf from v0   
-    abs_minPos_to_vf, acc_to_vf, t_jrk_to_vf, t_acc_to_vf = calculate_minPos_reachAcc_maxJrkTime_maxAccTime_to_final_vel(abs_v_start, abs_v_end, abs_max_vel, abs_max_acc, abs_max_jrk)
-    if abs_minPos_to_vf > abs(p_end-p_start) and  abs_minPos_to_vf - abs(p_end-p_start) > 1e-5: # if abs_minPos_to_vf> abs(p_end-p_start), then these values are not feasible
-        print">>> min required position difference to reach v_end from v_start= {} > abs(p_end-p_start)={} ".format(abs_minPos_to_vf, abs(p_end-p_start) )       
+    abs_min_pos_to_vf, acc_to_vf, t_jrk_to_vf, t_acc_to_vf = calculate_min_pos_reached_acc_jrk_time_acc_time_to_reach_final_vel(abs_v_start, abs_v_end, abs_max_vel, abs_max_acc, abs_max_jrk)
+    if abs_min_pos_to_vf > abs(p_end-p_start) and  abs_min_pos_to_vf - abs(p_end-p_start) > 1e-5: # if abs_min_pos_to_vf> abs(p_end-p_start), then these values are not feasible
+        print">>> min required position difference to reach v_end from v_start= {} > abs(p_end-p_start)={} ".format(abs_min_pos_to_vf, abs(p_end-p_start) )       
         raise ValueError("non feasible case: violate min_pos_to_vf" )      
         return 0, 0, 0, 0, 0
     
@@ -288,10 +320,10 @@ def traj_segment_planning(p_start, p_end, abs_v_start, abs_v_end, abs_max_vel, a
             abs_v=abs_v_start
         
         
-        if abs( abs(p_end-p_start) - abs_minPos_to_vf)> 1e-7:
-            pos_diff= p_end - p_start - math.copysign( abs_minPos_to_vf, p_end - p_start)   
+        if abs( abs(p_end-p_start) - abs_min_pos_to_vf)> 1e-7:
+            pos_diff= p_end - p_start - math.copysign( abs_min_pos_to_vf, p_end - p_start)   
             ## plan the rest of the motion using the equal start/end vel case
-            t_jrk, t_acc, t_vel, localMaxVel, localMaxAcc = equal_vel_case_planning ( abs(pos_diff), abs_v, abs_max_vel, abs_max_acc, abs_max_jrk)
+            t_jrk, t_acc, t_vel, reached_vel, reached_acc = equal_vel_case_planning ( abs(pos_diff), abs_v, abs_max_vel, abs_max_acc, abs_max_jrk)
         else:
             t_jrk=0.0
             t_acc=0.0
