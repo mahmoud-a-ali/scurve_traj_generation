326f33a7816c69a4c9a39a95d9b8f0db69be1f7e
mahmoud-a-ali <mahmud.a.aly@gmail.com>
2020-03-25 15:58:32 +0100

    spaces

--- traj/src/traj/__init__.py
+++ traj/src/traj/__init__.py
@@ -13,26 +13,18 @@ from traj_segment import calculate_jerk_sign_and_duration
 from segment_planning import traj_segment_planning
 from segment_planning import calculate_min_pos_reached_acc_jrk_time_acc_time_to_reach_final_vel
 
+from sample_segment import sample_segment
 from plot_traj_segment import plot_traj_segment
-
 
 from cubic_eq_roots import real_roots_cubic_eq
 from cubic_eq_roots import quad_eq_real_root
 from cubic_eq_roots import min_positive_root2
 from cubic_eq_roots import min_positive_root3
 
-
-
 from max_reachable_vel import max_reachable_vel_per_segment
-
-
 from param_max_reachable_vel import set_velocities_at_stop_points_to_zero
 from param_max_reachable_vel import reachable_vel_at_each_waypoint_one_dof_path_case
 from param_max_reachable_vel import reachable_vel_at_each_waypoint_multi_dof_path_case
-
-
-from sample_segment import sample_segment
-
 
 from synchronize_joint_motion import synchronize_joint_motion
 from synchronize_joint_motion import motion_direction
