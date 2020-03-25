11295c4cd1ce72504574bcc760dd77eaedece7a9
mahmoud-a-ali <mahmud.a.aly@gmail.com>
2020-03-25 15:59:43 +0100

    variable: small_letters\underscore

--- traj/src/traj/param_max_reachable_vel.py
+++ traj/src/traj/param_max_reachable_vel.py
@@ -14,7 +14,6 @@ def set_velocities_at_stop_points_to_zero(path):
     arr = np.array(wpts_vel_dir)
     stp_pts_idx = np.where(arr==0)[0]
     return wpts_vel_dir, stp_pts_idx
-    
 
     
 def max_vel_at_each_waypoint_one_dof_path_case(path, v_init, abs_max_pos, abs_max_vel, abs_max_acc, abs_max_jrk):
@@ -25,18 +24,15 @@ def max_vel_at_each_waypoint_one_dof_path_case(path, v_init, abs_max_pos, abs_ma
     stp_idx =  [id for id in stp_idx] 
     n_wpts = len(path) 
     max_vel_vec = [ abs(v_init)   ]  
-    ### estimate max reachable velocity
     for wpt in range(0, n_wpts-1 ) :
         if wpt+1 in stp_idx:
             v_nxt = 0.0
         else:
             tj, ta, tv, v_nxt = traj.max_reachable_vel_per_segment( abs(path[wpt] - path[wpt+1]), max_vel_vec[wpt], abs_max_pos, abs_max_vel, abs_max_acc, abs_max_jrk)
-            #print "abs_pos_diff={}, v_forward={}, nxt_v={} ".format(abs(path[wpt] - path[wpt+1]), v_forward, v_frwrd)        
         max_vel_vec.append(  v_nxt )      
     return max_vel_vec
         
-
-      
+ 
 def reachable_vel_at_each_waypoint_one_dof_path_case(path, v_start, v_end, abs_max_pos, abs_max_vel, abs_max_acc, abs_max_jrk):
     ''' 
     this function finds the estimated reachable velocity at each waypoint along a 1-dof path "path", with starting velocity "v_init", a final velocity "v_end"   
@@ -46,18 +42,15 @@ def reachable_vel_at_each_waypoint_one_dof_path_case(path, v_start, v_end, abs_m
     frwd_max_vel = max_vel_at_each_waypoint_one_dof_path_case(path, v_start, abs_max_pos, abs_max_vel, abs_max_acc, abs_max_jrk)
     bkwd_max_vel = max_vel_at_each_waypoint_one_dof_path_case(path[::-1], v_end, abs_max_pos, abs_max_vel, abs_max_acc, abs_max_jrk)  #L[::-1]
     bkwd_max_vel.reverse()
-    
-    ##check condition when v_start or v_end is not feasible: v_start > max_v_start calculated using the backward loop or Vs
+    # check condition when v_start or v_end is not feasible: v_start > max_v_start calculated using the backward loop or Vs
     if frwd_max_vel[0] > bkwd_max_vel[0] or frwd_max_vel[-1] < bkwd_max_vel[-1]:
         raise ValueError("combination of v_start({}) & v_end({}) is not feasible".format(frwd_max_vel[0], bkwd_max_vel[-1] ) )                 
     # calcuate max_rechable_vels that grantee v_end at the end of the trajectory for this portion of traj
     estimated_vel =  [ min(v) for v in zip( frwd_max_vel, bkwd_max_vel)] 
-    ## retrieve the direction at each way point 
+    # retrieve the direction at each way point 
     wpts_vel_dir, stp_idx= traj.set_velocities_at_stop_points_to_zero(path)
     estimated_vel=[v*dir for v, dir in zip(estimated_vel, wpts_vel_dir) ]
     return frwd_max_vel, bkwd_max_vel, estimated_vel
-
-
 
 
 def reachable_vel_at_each_waypoint_multi_dof_path_case(path, v_start, v_end, abs_max_pos, abs_max_vel, abs_max_acc, abs_max_jrk):
@@ -74,40 +67,3 @@ def reachable_vel_at_each_waypoint_multi_dof_path_case(path, v_start, v_end, abs
         frwd_max_vel, bkwd_max_vel, estimated_vel =  reachable_vel_at_each_waypoint_one_dof_path_case(path[pth], v_start[pth], v_end[pth], abs_max_pos, abs_max_vel, abs_max_acc, abs_max_jrk)
         reachable_vel.append( estimated_vel )
     return reachable_vel
-    
-  
-  
-  
-
-
-
-
-
-
-
-
-
-
-
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-    
