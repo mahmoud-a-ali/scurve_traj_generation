7ae55c517a3d1531dc7f5584b1138c3966e7eaf6
mahmoud-a-ali <mahmud.a.aly@gmail.com>
2020-04-22 16:26:05 +0200

    add n_dof segment_synchronization

--- traj/src/traj/synchronize_joint_motion.py
+++ traj/src/traj/synchronize_joint_motion.py
@@ -7,7 +7,7 @@ import rospy
 
 def synchronize_joint_motion(t_syn, pos_diff, v_start, v_end, abs_max_pos, abs_max_vel, abs_max_acc, abs_max_jrk):
 	'''
-	this function selects a motion profile for  a general trajectory segment considering the total motion time of the segment is "t_syn"
+	this function selects a motion profile for a general trajectory segment considering the total motion time of the segment is "t_syn"
 	it returns the jerk_value && duration associated with each phase of the segment
 	it raise an error if:
 		1. if the given time "t_syn" is less than the minimum time caclulated using the maximum jerk 
@@ -98,3 +98,62 @@ def motion_direction( v_start, v_end, pos_diff):
 	else:
 		raise ValueError("identify_motion_direction: motion is not feasible")
 		return 0 
+
+
+def segment_synchronization(pos_start, pos_end, vel_start, vel_end, 
+	                        abs_max_pos, abs_max_vel, abs_max_acc, abs_max_jrk):
+	'''
+	A high level segment synchronization function based on the "synchronize_joint_motion" function.
+	it is used to synchronize n-dof segment!
+	this function is based on the same idea of the paper entitled : 
+	Online Trajectory Generation: Basic Concepts for Instantaneous Reactions to Unforeseen Events[1], 
+	section V,  synchronization steps 1,2,3
+	[1] https://www-cs.stanford.edu/groups/manips/publications/pdfs/Kroeger_2010_TRO.pdf
+	'''
+	rospy.logdebug(">> pos_start:\n{}".format(pos_start))
+	rospy.logdebug(">> pos_end:\n{}".format(pos_end))
+	rospy.logdebug(">> vel_start:\n{}".format(vel_start))
+	rospy.logdebug(">> vel_end:\n{}".format(vel_end))
+	pos_diff = [pf-pi for pi, pf in zip(pos_start, pos_end)]
+	motion_dir = [] 
+	n_jts = len(pos_diff)
+	for jt in range(n_jts):
+		motion_dir.append(traj.motion_direction(vel_start[jt],  vel_end[jt], pos_diff[jt]))
+
+	# step 1: find the minimum time motion for each joints 
+	min_motion_time  = [ ]
+	for jt in range(n_jts):
+		# min time for each segment: phases times
+		tj_2vf, ta_2vf, t_jrk, t_acc, t_vel = traj.traj_segment_planning(0.0, abs(pos_diff[jt]), abs(vel_start[jt]), abs(vel_end[jt]),
+																		 abs_max_vel[jt], abs_max_acc[jt], abs_max_jrk[jt])
+		min_time = 2*tj_2vf + ta_2vf +  4*t_jrk + 2*t_acc + t_vel
+		min_motion_time.append(min_time) 
+
+	# step 2: find the joint that has the maximum time motion (reference joint)
+	ref_jt = min_motion_time.index(max(min_motion_time))
+	min_sync_time  = max(min_motion_time) 
+	syn_t = min_sync_time
+	rospy.logdebug(">> syn_t : {} ".format(syn_t))
+	rospy.logdebug(">> ref_jt: {} ".format(ref_jt))
+	rospy.logdebug(">> min_T : {} ".format(min_motion_time))
+
+	# step 3: calculate new jrk_sgn_dur
+	phase_dur_jt = []
+	phase_jrk_jt = []
+	for jt in range(n_jts):
+		rospy.logdebug("\n\n>> jt:{}, PD: {}, v_start:{}, v_end:{}".format(jt, pos_diff[jt], vel_start[jt], vel_end[jt]))
+		p_diff = abs(pos_diff[jt])   
+		v_start = abs(vel_start[jt])
+		v_end = abs(vel_end[jt])
+		if jt == ref_jt:
+			jrk_sign_dur = traj.calculate_jerk_sign_and_duration(0.0, p_diff, v_start, v_end, 
+								abs_max_pos[jt], abs_max_vel[jt], abs_max_acc[jt], abs_max_jrk[jt])
+		else:
+			jrk_sign_dur = synchronize_joint_motion(syn_t, p_diff, v_start, v_end, 
+								abs_max_pos[jt], abs_max_vel[jt], abs_max_acc[jt], abs_max_jrk[jt])
+		dur = [jsd[1] for jsd in jrk_sign_dur]
+		jrk = [motion_dir[jt]*jsd[0] for jsd in jrk_sign_dur]
+		phase_dur_jt.append(dur)
+		phase_jrk_jt.append(jrk)
+		rospy.logdebug(">> dur:{}".format(sum(dur)))
+	return min_sync_time, phase_dur_jt, phase_jrk_jt
